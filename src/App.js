568291217fef1dce49ed4c6b898505c39a5352c5
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-10-25 15:14:19 +0900

    import modules and modify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+
+import styles from "./App.module.css";
+import Auth from "./components/Auth";
+import Mainpage from "./components/Mainpage";
+
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:8000/graphql/",
+  headers: {
+    authorization: localStorage.getItem("token")
+      ? `JWT ${localStorage.getItem("token")}`
+      : "",
+  },
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className={styles.app}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Auth} />
+            <Route exact path="/employees" component={Mainpage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
   );
 }
 
