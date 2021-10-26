936d94ed8ab816c386a4cfeb969c5d09f992cd9a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-10-26 11:01:58 +0900

    import modules

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+import styles from "./MainPage.module.css";
+import { GET_EMPLOYEES } from "../Queries";
 
 const Mainpage = () => {
   return <div></div>;
