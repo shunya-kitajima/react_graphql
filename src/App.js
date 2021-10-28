619ff9de68d7403271b54497efc77e4a7025a04d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-10-28 17:37:22 +0900

    import StateContextProvider

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, BrowserRouter, Switch } from "react-router-dom";
 import styles from "./App.module.css";
 import Auth from "./components/Auth";
 import Mainpage from "./components/Mainpage";
+import StateContextProvider from "./context/StateContext";
 
 const client = new ApolloClient({
   uri: "http://127.0.0.1:8000/graphql/",
@@ -20,14 +21,16 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className={styles.app}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Auth} />
-            <Route exact path="/employees" component={Mainpage} />
-          </Switch>
-        </BrowserRouter>
-      </div>
+      <StateContextProvider>
+        <div className={styles.app}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Auth} />
+              <Route exact path="/employees" component={Mainpage} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </StateContextProvider>
     </ApolloProvider>
   );
 }
