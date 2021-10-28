8ef6dd2c489d9a5e06e0ddc92c0d065e8f5ea98d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-10-28 15:04:10 +0900

    add files

---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { useMutation } from "@apollo/client";
+
+import { StateContext } from "../context/StateContext";
+import styles from "./EmployeeCreate.module.css";
+
+const EmployeeCreate = () => {
+  return <div></div>;
+};
+
+export default EmployeeCreate;
