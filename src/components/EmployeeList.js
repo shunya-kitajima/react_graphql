18f0344e968ed0264d4d63a3dd4628c0bc699798
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-10-31 21:23:33 +0900

    add deleteEmployee

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import { useMutation } from "@apollo/client";
 
 import styles from "./EmployeeList.module.css";
+import { StateContext } from "../context/StateContext";
+import { GET_EMPLOYEES, DELETE_EMPLOYEE } from "../queries";
 
 const EmployeeList = ({ dataEmployees }) => {
+  const {
+    setName,
+    setJoinYear,
+    setSelectedDept,
+    setEditedId,
+    // dataSingleEmployee,
+    // getSingleEmployee,
+  } = useContext(StateContext);
+
+  const [deleteEmployee] = useMutation(DELETE_EMPLOYEE, {
+    refetchQueries: [{ query: GET_EMPLOYEES }],
+  });
+
   return (
     <>
       <h3>Employee List</h3>
@@ -17,6 +35,31 @@ const EmployeeList = ({ dataEmployees }) => {
                 {" / "}
                 {empl.node.department.deptName}
               </span>
+              <div>
+                <DeleteIcon
+                  className={styles.employeeList__delete}
+                  onClick={async () => {
+                    try {
+                      await deleteEmployee({
+                        variables: {
+                          id: empl.node.id,
+                        },
+                      });
+                    } catch (err) {
+                      alert(err.message);
+                    }
+                  }}
+                />
+                <EditIcon
+                  className={styles.employeeList__edit}
+                  onClick={() => {
+                    setEditedId(empl.node.id);
+                    setName(empl.node.name);
+                    setJoinYear(empl.node.joinYear);
+                    setSelectedDept(empl.node.department.id);
+                  }}
+                />
+              </div>
             </li>
           ))}
       </ul>
