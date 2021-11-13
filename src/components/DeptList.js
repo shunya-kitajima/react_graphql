f472a70f262460c998af9c13734a912467b306d4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-11-13 12:09:10 +0900

    create DeptList component

--- src/components/DeptList.js
+++ src/components/DeptList.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useContext } from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { useMutation } from "@apollo/client";
 
-const DeptList = () => {
-  return <div></div>;
+import { StateContext } from "../context/StateContext";
+import styles from "./DeptList.module.css";
+import { CREATE_DEPT, DELETE_DEPT, GET_DEPTS, GET_EMPLOYEES } from "../queries";
+
+const DeptList = ({ dataDepts }) => {
+  const { deptName, setDeptName } = useContext(StateContext);
+
+  const [createDept] = useMutation(CREATE_DEPT, {
+    refetchQueries: [{ query: GET_DEPTS }, { query: GET_EMPLOYEES }],
+  });
+
+  const [deleteDept] = useMutation(DELETE_DEPT, {
+    refetchQueries: [{ query: GET_DEPTS }, { query: GET_EMPLOYEES }],
+  });
+
+  return (
+    <>
+      <h3>Department List</h3>
+      <input
+        className={styles.deptList__input}
+        placeholder="new department name"
+        type="text"
+        value={deptName}
+        onChange={(e) => setDeptName(e.target.value)}
+      />
+      <button
+        disabled={!deptName}
+        onClick={async () => {
+          try {
+            await createDept({
+              variables: {
+                deptName: deptName,
+              },
+            });
+          } catch (err) {
+            alert(err.messae);
+          }
+          setDeptName("");
+        }}
+      >
+        New dept
+      </button>
+      <ul className={styles.deptList__list}>
+        {dataDepts &&
+          dataDepts.allDepartments &&
+          dataDepts.allDepartments.edges.map((dept) => (
+            <li className={styles.deptList__item} key={dept.node.id}>
+              <span>{dept.node.deptName}</span>
+              <div>
+                <DeleteIcon
+                  className={styles.deptList_delete}
+                  onClick={async () => {
+                    try {
+                      await deleteDept({
+                        variables: {
+                          id: dept.node.id,
+                        },
+                      });
+                    } catch (err) {
+                      alert(err.message);
+                    }
+                  }}
+                />
+              </div>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 
 export default DeptList;
